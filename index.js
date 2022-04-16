955bb586285e4f6bbf12d6c6ff9f2aadbde39ad3
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-16 14:45:39 -0400

    Add random-d.uk

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello, World!')
+const IMG = document.querySelector('img');
+const FETCH_BUTTON = document.querySelector('button');
+
+function fetchRandomDuck(){
+	return fetch('https://api.codetabs.com/v1/proxy?quest=https://random-d.uk/api/v2/random')
+		.then(response => response.json())
+		.then(data => {
+			IMG.src = data.url
+		})
+}
+
+FETCH_BUTTON.addEventListener('click', () => fetchRandomDuck())
