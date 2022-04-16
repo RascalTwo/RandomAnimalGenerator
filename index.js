aa4c5594024c3f7c432573eddaf8b818bcc41971
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-16 14:55:07 -0400

    Add axoltlapi

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const IMG = document.querySelector('img');
 const FETCH_BUTTON = document.querySelector('button');
 
-function fetchRandomDuck(){
-	return fetch('https://api.codetabs.com/v1/proxy?quest=https://random-d.uk/api/v2/random')
+const URLS = [
+	'https://api.codetabs.com/v1/proxy?quest=https://axoltlapi.herokuapp.com/',
+	'https://api.codetabs.com/v1/proxy?quest=https://random-d.uk/api/v2/random'
+]
+
+function fetchRandomImage(){
+	const url = URLS[Math.floor(Math.random() * URLS.length)]
+	return fetch(url)
 		.then(response => response.json())
 		.then(data => {
 			IMG.src = data.url
 		})
 }
 
-FETCH_BUTTON.addEventListener('click', () => fetchRandomDuck())
+FETCH_BUTTON.addEventListener('click', () => fetchRandomImage())
